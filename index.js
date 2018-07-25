2acf39fc4b4aff32707ed56877a2f7bb612b1d13
Erik Thompson <xsanic@Palladium.local>
2018-07-24 20:54:46 -0400

    fix buildpack

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there'});
+});
+
+
+
+//Heroku Porting with condition
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+//Development porting
+//app.listen(5000);
