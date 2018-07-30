47fbec7d47a4e8fc8dd33bde621de8584558424d
Erik Thompson <xsanic@Palladium.local>
2018-07-30 10:04:55 -0400

    adding the logic for existing users

--- services/passport.js
+++ services/passport.js
@@ -10,9 +10,18 @@ passport.use(new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
-	}, 
+	},
 	(accessToken, refreshToken, profile, done) => {
-		new User({ googleID: profile.id}).save();
+		User.findOne({googleID: profile.id})
+		.then((existingUser) => {
+			if (existingUser){
+
+			}
+			else {
+					new User({ googleID: profile.id}).save();
+			}
+		})
+
 		//console.log('access token', accessToken);
 		//console.log('refresh token', refreshToken);
 		//console.log('profile',profile);
